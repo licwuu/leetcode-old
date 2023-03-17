dd2d6a527d55cb3c3f91d586f737739bbcea4d97
lcw <3115035620@qq.com>
2023-03-17 20:21:51 +0800

    该以前题解格式

--- editor/cn/CombinationSum39.java
+++ editor/cn/CombinationSum39.java
@@ -1,31 +1,35 @@
-package editor.cn.solution;
+package editor.cn;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-class CombinationSum{
+public class CombinationSum39 {
     public static void main(String[] args) {
-        Solution solution = new CombinationSum().new Solution();
+        Solution solution = new CombinationSum39().new Solution();
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
         private final List<List<Integer>> ans = new ArrayList<>();
+
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
             Arrays.sort(candidates);
             helper(new ArrayList<>(), candidates, 0, 0, target);
             return ans;
         }
 
-        void helper(List<Integer> order, int[] candidates,int index, int sum, int target){
-            for(int i = index; i < candidates.length; i++){
+        void helper(List<Integer> order, int[] candidates, int index, int sum, int target) {
+            for (int i = index; i < candidates.length; i++) {
                 int candidate = candidates[i];
-                if(sum + candidate > target) break;
-                if(sum + candidate == target){
+                if (sum + candidate > target) break;
+                if (sum + candidate == target) {
                     order.add(candidate);
                     ans.add(new ArrayList<>(order));
-                }else if(sum + candidate < target){
+                } else if (sum + candidate < target) {
                     order.add(candidate);
-                    helper(order, candidates, i, sum+candidate, target);
+                    helper(order, candidates, i, sum + candidate, target);
                 }
                 order.remove(order.size() - 1);
             }
@@ -33,6 +37,6 @@ class CombinationSum{
 
         }
     }
-    //leetcode submit region end(Prohibit modification and deletion)
+//leetcode submit region end(Prohibit modification and deletion)
 
 }
