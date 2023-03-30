a88784da2c74c529c8b2c2db3f5c0a2ef7e66c59
lcw <3115035620@qq.com>
2023-03-30 20:11:03 +0800

    leetcode:2023.3.24

---
+++ editor/cn/ShortestSubarrayToBeRemovedToMakeArraySorted1574.java
@@ -0,0 +1,17 @@
+package editor.cn;
+
+public class ShortestSubarrayToBeRemovedToMakeArraySorted1574 {
+    public static void main(String[] args) {
+        Solution solution = new ShortestSubarrayToBeRemovedToMakeArraySorted1574().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findLengthOfShortestSubarray(int[] arr) {
+
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ editor/cn/StreamOfCharacters1032.java
@@ -0,0 +1,23 @@
+package editor.cn;
+
+public class StreamOfCharacters1032 {
+    public static void main(String[] args) {
+        StreamOfCharacters1032 streamOfCharacters = new StreamOfCharacters1032();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class StreamChecker {
+
+        public StreamChecker(String[] words) {
+
+        }
+
+        public boolean query(char letter) {
+            return false;
+        }
+    }
+
+
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
